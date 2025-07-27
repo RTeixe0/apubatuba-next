e3d1369be3fbaad10c74e1cd50ce08d8dd2f927d
RTeixe0 <renanteixeira121@outlook.com>
2025-07-27 12:22:08 -0300

    SEO: Adiciona verificação do Google e ajustes no _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,13 +2,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="pt-BR">
+      <Head>
         <meta
           name="google-site-verification"
           content="J3QPGyjzLX5BKieF-b8qE7-_93zc-Mww8Xk-XS0Lssk"
         />
+
+        {/* SEO técnico */}
+        <meta charSet="UTF-8" />
+        <meta name="theme-color" content="#003c63" />
+
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
