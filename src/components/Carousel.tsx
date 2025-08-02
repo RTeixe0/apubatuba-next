084e480bcd36231f3628b391195b3e6b091ecbff
RTeixe0 <renanteixeira121@outlook.com>
2025-08-01 23:13:09 -0300

    Alterado proporção carrocel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -23,24 +23,28 @@ export function Carousel({ pasta, prefixo, total, slug }: CarouselProps) {
     <section className="carousel scroll-animate py-8">
       <div className="container mx-auto max-w-4xl px-4">
         <Swiper
-          key={slug} // ✅ forçando reset ao trocar de apê
+          key={slug}
           modules={[Navigation, Pagination, Autoplay]}
           loop
           autoplay={{ delay: 4000, disableOnInteraction: false }}
           pagination={{ clickable: true }}
           navigation
-          className="rounded-xl overflow-hidden bg-[#00263e]"
+          className="rounded-xl overflow-hidden"
         >
           {imagens.map((img, index) => (
             <SwiperSlide key={index}>
-              <Image
-                src={img.src}
-                alt={img.alt}
-                width={900}
-                height={600}
-                className="w-full h-[450px] object-contain"
-                loading="lazy"
-              />
+              <div className="relative w-full aspect-[16/9]">
+                {" "}
+                {/* controla o tamanho com proporção */}
+                <Image
+                  src={img.src}
+                  alt={img.alt}
+                  fill
+                  className="object-cover rounded-xl"
+                  sizes="(max-width: 768px) 100vw, 900px"
+                  loading="lazy"
+                />
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
