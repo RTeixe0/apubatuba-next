d63fab75a027bf54907cc5c400a7e041c2d10a9d
RTeixe0 <renanteixeira121@outlook.com>
2025-08-02 12:24:54 -0300

    retirado www da url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.apubatubapraiagrande.com.br", // domínio correto
+  siteUrl: "https://apubatubapraiagrande.com.br", // domínio correto
   generateRobotsTxt: true,
   sitemapSize: 5000,
   additionalPaths: async (config) => [
